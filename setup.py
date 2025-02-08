9a75ed41d5b650246831e035e53b490f8b9ab082
Ryan Punamiya <38972198+ryanthecreator@users.noreply.github.com>
2025-02-08 17:02:21 -0500

    modify refs for egoverse repo

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     name="egomimic",
     version="1.0",
     description="Egomimic Scripts Package",
-    author="Simar Kareer",
-    author_email="skareer@gatech.edu",
+    author="EgoMimic-Team",
+    author_email="",
     url="https://egomimic.github.io",
     packages=find_packages(),  # Automatically discovers all packages
 )
